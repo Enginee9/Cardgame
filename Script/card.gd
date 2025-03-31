7be25e507d206fd8e5ea58956618b1f41111da76
123129 <hohocheung268@gmail.com>
2025-04-01 05:44:06 +0800

    latest

--- Script/card.gd
+++ Script/card.gd
@@ -1,95 +1,43 @@
 extends Area2D
 
-# Signals
-signal active_card(node: Node)
-signal card_selected(node: Node)
+var cardname
+var cardvalue
+var cardsuit
+var dealt = false
 
-# Exported variables (Godot 4 syntax)
-@export var focus_move_on_y: int = 40
 @export var cardsprite: Texture2D
-@export var cardscale: Vector2 = Vector2(1, 1):
+var cardscale: Vector2:       # setget in 4.0 version
 	set(value):
+		cardscale = value 
 		change_cardscale(value)
+		
+var handposition = Vector2.ZERO
+var handrotation = Vector2.ZERO
 
-# Regular variables
-var card_name: String
-var card_value: int
-var card_suit: String
-var dealt: bool = false
-var selectable: bool = false:
-	set(value):
-		set_selectable(value)
-var selected_card: bool = false
-var front_sprite_path: String
-var cardowner
-var touchable: bool = true
+func move_card(dest, rotate = null, _scale = null):
+		$Tween.interpolate_property(self, "position" , position, dest, 0.7, Tween.TRANS_BACK, Tween.EASE_OUT)
+		if rotate != null:
+			$Tween.interpolate_property(self, "rotation", rotation, rotate, 0.7, Tween.TRANS_LINEAR, Tween.EASE_IN)
+		if _scale != null:
+			$Tween.interpolate_property(self, "scale", scale, _scale, 0.7, Tween.TRANS_LINEAR, Tween.EASE_IN)
+		$Tween.start()
 
-var handposition: Vector2 = Vector2.ZERO
-var handrotation: float = 0.0
-
-func set_selectable(val: bool) -> void:
-	selectable = val
-
-func move_card(dest: Vector2, rotate: Variant = null, _scale: Variant = null) -> void:
-	var tween = create_tween()
-	tween.tween_property(self, "position", dest, 0.5)\
-		.set_trans(Tween.TRANS_BACK)\
-		.set_ease(Tween.EASE_OUT)
-	
-	if rotate != null:
-		tween.parallel().tween_property(self, "rotation", rotate, 0.2)\
-			.set_trans(Tween.TRANS_LINEAR)\
-			.set_ease(Tween.EASE_IN)
-	
-	if _scale != null:
-		tween.parallel().tween_property(self, "scale", _scale, 0.5)\
-			.set_trans(Tween.TRANS_BACK)\
-			.set_ease(Tween.EASE_OUT)
-
-func change_sprite(res: String) -> void:
+func change_sprite(res):
 	$Sprite.texture = load(res)
 
-func change_cardscale(_scale: Vector2) -> void:
+func change_cardscale(_scale):
 	scale = _scale
 
-func card_width() -> float:
-	return $Sprite.texture.get_width() * scale.x
+func card_width():
+	var cardwidth = $Sprite.texture.get_width() * scale.x
+	return cardwidth
 
-func kill_card() -> void:
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func kill_card():
 	queue_free()
-
-func make_focus() -> void:
-	if selectable:
-		var position_shift = position
-		position_shift.y -= focus_move_on_y
-		if position == handposition:
-			move_card(position_shift, 0.0)
-		z_index = 2
-		selected_card = true
-		emit_signal("active_card", self)
-
-func off_focus() -> void:
-	if selectable:
-		move_card(handposition, handrotation)
-		z_index = 1
-		selected_card = false
-
-func make_active(card: Node) -> void:
-	if card != self:
-		off_focus()
-
-func _on_card_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if touchable:
-		if event is InputEventMouseButton or event is InputEventScreenTouch:
-			if (event.is_action_pressed("left_click") and selected_card or (event.is_pressed() and selected_card)):
-				selected_card = false
-				emit_signal("card_selected", self)
-				touchable = false
-				$TouchTimer.start()
-			elif (not selected_card and event.is_action_pressed("left_click")) or (not selected_card and event.is_pressed()):
-				touchable = false
-				$TouchTimer.start()
-				make_focus()
-
-func _on_touch_timer_timeout() -> void:
-	touchable = true
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
